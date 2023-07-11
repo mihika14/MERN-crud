eb8ce8254f713bb8ad500ecfd68e9c1885051f36
mihika14 <mihikasaxena13@gmail.com>
2023-07-11 16:41:16 +0530

    first commit

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <button className="button"> add data</button>
+    </div>
+  );
+}
+
+export default HomePage;
